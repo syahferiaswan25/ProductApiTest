258d05b42ce347b0cbff16f837f08a37788b322e
syahferiaswan <muhamad.aswan@axa-mandiri.co.id>
2026-04-09 00:59:39 +0700

    deploy attempt 2

--- obj/Debug/net8.0/ProductApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProductApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1f5de9ce9bc388b6717196ccbb2f6afaf1db9a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdc335242d0b51899571dac5025feb0b0c8df381")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
